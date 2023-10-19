6cd38a20a93e51342e63955b4f19733ea9f9e7bb
knm22mukk <knm22mukk@gmail.com>
2023-10-19 12:31:13 +0900

    ページのタイトルを変更、デプロイ確認

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import '@/globals.css';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'おとちブログ',
-  description: '包装資材に関する情報を配信するおとちの個人ブログです',
+  title: '大黒埼玉',
+  description: '大黒埼玉の特設サイトです',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
